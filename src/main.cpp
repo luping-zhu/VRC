68ddc1758c85b7bdb77c63ae08dc563ebb8c966f
Luping Zhu <lupingzhu@yahoo.com>
2024-07-04 20:02:58 -0700

    This is the first commit for this blank project template

---
+++ src/main.cpp
@@ -0,0 +1,138 @@
+#include "main.h"
+
+/* Documentation */
+// https://ez-robotics.github.io/EZ-Template/
+
+// Chassis constructor, edit accordingly
+ez::Drive chassis(
+    // These are your drive motors, the first motor is used for sensing!
+    {1, 2, 3},  // Left Chassis Ports, (use negative numbers for reversed motors!)
+    {4, 5, 6},  // Right Chassis Ports (use negative numbers for reversed motors!)
+
+    7,          // IMU (inertial) port
+    4.125,      // Wheel Diameter (Remember, 4" wheels without screw holes are actually 4.125!)
+    343         // Wheel RPM
+);
+
+/**
+ * Runs initialization code. This occurs as soon as the program is started.
+ *
+ * All other competition modes are blocked by initialize; it is recommended
+ * to keep execution time for this mode under a few seconds.
+ */
+void initialize()
+{
+  ez::ez_template_print();
+
+  pros::delay(500);  // Stop the user from doing anything while legacy ports configure
+
+  // Configure your chassis controls\
+
+  // Enables modifying the controller curve with buttons on the joysticks
+  chassis.opcontrol_curve_buttons_toggle(true);
+
+  // Sets the active brake kP. We recommend ~2.  0 will disable.
+  chassis.opcontrol_drive_activebrake_set(2);
+
+  // Defaults for curve. If using tank, only the first parameter is used.
+  // (Comment this line out if you have an SD card!) 
+  chassis.opcontrol_curve_default_set(0, 0);
+
+  // Set the constants using the function defined in autons.cpp
+  default_constants();
+
+  // Autonomous Selector
+  ez::as::auton_selector.autons_add(
+    {
+      // Add autons selections here
+    }
+  );
+
+  // Initialize chassis and auton selector
+  chassis.initialize();
+  ez::as::initialize();
+  master.rumble(".");
+}
+
+/**
+ * Runs while the robot is in the disabled state of Field Management System or
+ * the VEX Competition Switch, following either autonomous or opcontrol. When
+ * the robot is enabled, this task will exit.
+ */
+void disabled() 
+{
+  // Add your code here
+}
+
+/**
+ * Runs after initialize(), and before autonomous when connected to the Field
+ * Management System or the VEX Competition Switch. This is intended for
+ * competition-specific initialization routines, such as an autonomous selector
+ * on the LCD.
+ *
+ * This task will exit when the robot is enabled and autonomous or opcontrol
+ * starts.
+ */
+void competition_initialize() 
+{
+  // Add your code here
+}
+
+/**
+ * Runs the user autonomous code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the autonomous
+ * mode. Alternatively, this function may be called in initialize or opcontrol
+ * for non-competition testing purposes.
+ *
+ * If the robot is disabled or communications is lost, the autonomous task
+ * will be stopped. Re-enabling the robot will restart the task, not re-start it
+ * from where it left off.
+ */
+void autonomous() 
+{
+  chassis.pid_targets_reset();                // Resets PID targets to 0
+  chassis.drive_imu_reset();                  // Reset gyro position to 0
+  chassis.drive_sensor_reset();               // Reset drive sensors to 0
+  chassis.drive_brake_set(MOTOR_BRAKE_HOLD);  // Set motors to hold. This helps autonomous consistency
+
+  // Calls selected auton from autonomous selector
+  ez::as::auton_selector.selected_auton_call();
+}
+
+/**
+ * Runs the operator control code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the operator
+ * control mode.
+ *
+ * If no competition control is connected, this function will run immediately
+ * following initialize().
+ *
+ * If the robot is disabled or communications is lost, the
+ * operator control task will be stopped. Re-enabling the robot will restart the
+ * task, not resume it from where it left off.
+ */
+void opcontrol() 
+{
+  // This is preference to what you like to drive on
+  // MOTOR_BRAKE_HOLD (recommended), MOTOR_BRAKE_BRAKE, MOTOR_BRAKE_COAST
+  pros::motor_brake_mode_e_t driver_preference_brake = MOTOR_BRAKE_HOLD;
+  chassis.drive_brake_set(driver_preference_brake);
+
+  while (true) 
+  {
+    // chassis.opcontrol_tank();  // Tank control
+    // chassis.opcontrol_arcade_standard(ez::SPLIT);   // Standard split arcade
+    chassis.opcontrol_arcade_standard(ez::SINGLE);  // Standard single arcade
+    // chassis.opcontrol_arcade_flipped(ez::SPLIT);    // Flipped split arcade
+    // chassis.opcontrol_arcade_flipped(ez::SINGLE);   // Flipped single arcade
+
+    // . . .
+    // Put more user control code here!
+    // . . .
+
+    // This is used for timer calculations!  Keep this ez::util::DELAY_TIME
+    pros::delay(ez::util::DELAY_TIME);
+  }
+}
