c88829c0e9d8dca390674ec4477bfe88f6428639
Luping Zhu <lupingzhu@yahoo.com>
2024-10-12 12:01:32 -0700

    Fix include

--- include/subsystems.hpp
+++ include/subsystems.hpp
@@ -4,11 +4,11 @@
 
 
 inline pros::MotorGroup intake({-20, -8}); 
-inline ez::Piston piston_mobile('H', false);
 inline ez::Piston piston_side('B', false);
 inline ez::Piston piston_top('C', false);
+inline ez::Piston piston_mobile('E', false);
 
-inline int INTAKE_SPEED = 90;
+inline int INTAKE_SPEED = 100;
 inline int AUTON_SPEED = 75;
 inline int TURN_SPEED2 = 50;
 // Your non-drivetrain motors, sensors, etc. should go here. Below are examples:
